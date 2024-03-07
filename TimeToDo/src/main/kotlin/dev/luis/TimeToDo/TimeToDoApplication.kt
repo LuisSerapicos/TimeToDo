c246a536b261c25b0c089f15353460a8a5da5293
LuisGitBucket <luiserapicos18@gmail.com>
2024-03-07 05:10:05 +0000

    Initial spring boot project in Kotlin for the backend and added use case diagram.

---
+++ TimeToDo/src/main/kotlin/dev/luis/TimeToDo/TimeToDoApplication.kt
@@ -0,0 +1,11 @@
+package dev.luis.TimeToDo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimeToDoApplication
+
+fun main(args: Array<String>) {
+	runApplication<TimeToDoApplication>(*args)
+}
---
+++ TimeToDo/src/test/kotlin/dev/luis/TimeToDo/TimeToDoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.luis.TimeToDo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TimeToDoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
